63a0803768179496c8510b5c36700b359027863c
JUANMARC123 <30juanmarcossipac@gmail.com>
2026-06-10 23:39:40 -0400

    se elimino los archivos faltantes

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -8,22 +8,6 @@ class DatabaseSeeder extends Seeder
 {
     public function run()
     {
-        $this->call([
-            UserSeeder::class,
-            RolSeeder::class,
-            PermisoSeeder::class,
-            CategoriaSeeder::class,
-            LaboratorioSeeder::class,
-            PrincipioActivoSeeder::class,
-            FormaFarmaceuticaSeeder::class,
-            RutaSeeder::class,
-            RepartidorSeeder::class,
-            FarmaciaSeeder::class,
-            ProductoSeeder::class,
-            VehiculoSeeder::class,
-            LoteSeeder::class,
-            InventarioSeeder::class,
-            DespachoSeeder::class,
-        ]);
+        
     }
 }
